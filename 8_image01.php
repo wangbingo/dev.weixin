994d6626bc5449c4e01c2435469a6af35a1c82e8
wangbing <280877288@qq.com>
2015-05-12 18:37:14 +0800

    no message

---
+++ 8_image01.php
@@ -0,0 +1,86 @@
+﻿<?php
+/**
+  * wechat php test
+  */
+
+//define your token
+define("TOKEN", "weixin");
+$wechatObj = new wechatCallbackapiTest();
+//$wechatObj->valid();
+$wechatObj->responseMsg();
+
+class wechatCallbackapiTest
+{
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+        	echo $echoStr;
+        	exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+                
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $msgType = $postObj->MsgType;
+
+
+                $textTpl = " <xml>
+ 															<ToUserName><![CDATA[%s]]></ToUserName>
+  														<FromUserName><![CDATA[%s]]></FromUserName>
+  														<CreateTime>%s</CreateTime>
+  														<MsgType><![CDATA[news]]></MsgType>
+  														<ArticleCount>2</ArticleCount>
+  															<Articles>
+  															  <item>
+  															    <Title><![CDATA[图片一]]></Title> 
+  															    <Description><![CDATA[图片一描述]]></Description>
+  															    <PicUrl><![CDATA[http://www.image1.cn/upload/docnews/12/20130527/12_1369639156.jpg]]></PicUrl>
+  															    <Url><![CDATA[http://www.image1.cn/h/12/20130521/14685_5.html]]></Url>
+  															  </item>
+  															  <item>
+  															    <Title><![CDATA[图片二]]></Title> 
+  															    <Description><![CDATA[图片二描述]]></Description>
+  															    <PicUrl><![CDATA[http://www.image1.cn/upload/docnews/12/20130521/p17r36qv371jpt1j9u17kp49f15ub6.jpg]]></PicUrl>
+  															    <Url><![CDATA[http://www.image1.cn/h/12/20130521/14685_3.html]]></Url>
+  															  </item>
+  														  </Articles>
+   														<FuncFlag>1</FuncFlag>
+  													 </xml> "; 
+								$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
+						    echo $resultStr; 
+    }
+  }
+		
+	private function checkSignature()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
--- wx_sample.php
+++ wx_sample.php
@@ -2,9 +2,11 @@
 /**
   * wechat php test
   */
-
+require_once("common/global.php");
+global $Config;
+define("MYSQLPWD", $Config["DB_PWD"]); 
 //define your token
-define("TOKEN", "weixin");
+define("TOKEN", "weixin888");
 $wechatObj = new wechatCallbackapiTest();
 //$wechatObj->valid();
 $wechatObj->responseMsg();
@@ -24,6 +26,9 @@ class wechatCallbackapiTest
 
     public function responseMsg()
     {
+    $con=mysql_connect("127.0.0.1", "root", MYSQLPWD);
+		mysql_select_db("weixin") or die("Unable to select database");
+		mysql_query("SET NAMES UTF8");    		
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
@@ -34,32 +39,77 @@ class wechatCallbackapiTest
                 $fromUsername = $postObj->FromUserName;
                 $toUsername = $postObj->ToUserName;
                 $msgType = $postObj->MsgType;
+                 
+                if ($msgType=="image"){
+		
+						        $textTpl = "<xml>
+																<ToUserName><![CDATA[%s]]></ToUserName>
+																<FromUserName><![CDATA[%s]]></FromUserName>
+																<CreateTime>%s</CreateTime>
+																<MsgType><![CDATA[%s]]></MsgType>
+																<Content><![CDATA[%s]]></Content>
+																<FuncFlag>0</FuncFlag>
+																</xml>";                 
+		
+		
+											$contentStr = "欢迎来到微信MOA系统：\r\n\r\n您刚才上传的图片已经保存在微信服务器上，浏览地址是：".$postObj->PicUrl; 
+						          $time = time();
+						          $msgType = "text";
+						          $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+						          echo $resultStr;	
+                	
+                }  
+                 
+                if ($msgType=="text"){ 
+                   $keyword = $postObj->Content;
+                   $CreateTime = time();
+                   $FuncFlag =  1 ;
 
 
-                $textTpl = " <xml>
- 															<ToUserName><![CDATA[%s]]></ToUserName>
-  														<FromUserName><![CDATA[%s]]></FromUserName>
-  														<CreateTime>%s</CreateTime>
-  														<MsgType><![CDATA[news]]></MsgType>
-  														<ArticleCount>2</ArticleCount>
-  															<Articles>
-  															  <item>
-  															    <Title><![CDATA[图片一]]></Title> 
-  															    <Description><![CDATA[图片一描述]]></Description>
-  															    <PicUrl><![CDATA[http://www.image1.cn/upload/docnews/12/20130527/12_1369639156.jpg]]></PicUrl>
-  															    <Url><![CDATA[http://www.image1.cn/h/12/20130521/14685_5.html]]></Url>
-  															  </item>
-  															  <item>
-  															    <Title><![CDATA[图片二]]></Title> 
-  															    <Description><![CDATA[图片二描述]]></Description>
-  															    <PicUrl><![CDATA[http://www.image1.cn/upload/docnews/12/20130521/p17r36qv371jpt1j9u17kp49f15ub6.jpg]]></PicUrl>
-  															    <Url><![CDATA[http://www.image1.cn/h/12/20130521/14685_3.html]]></Url>
-  															  </item>
-  														  </Articles>
-   														<FuncFlag>1</FuncFlag>
-  													 </xml> "; 
-								$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
-						    echo $resultStr; 
+                   if ($keyword=="景点"){  
+                    
+							        $newTplHeader = "<xml>
+							            <ToUserName><![CDATA[{$fromUsername}]]></ToUserName>
+							            <FromUserName><![CDATA[{$toUsername}]]></FromUserName>
+							            <CreateTime>{$CreateTime}</CreateTime>
+							            <MsgType><![CDATA[news]]></MsgType>
+							            <ArticleCount>%s</ArticleCount><Articles>";
+							        $newTplItem = "<item>
+							            <Title><![CDATA[%s]]></Title>
+							            <Description><![CDATA[%s]]></Description>
+							            <PicUrl><![CDATA[%s]]></PicUrl>
+							            <Url><![CDATA[%s]]></Url>
+							            </item>";
+							        $newTplFoot = "</Articles>
+							            <FuncFlag>%s</FuncFlag>
+							            </xml>"; 
+  													 
+  										
+  										$sql="select * from images order by ids limit 0,10";
+  										$result=mysql_query($sql);	
+  										$rsNextman=mysql_fetch_assoc($result);	
+  										
+  										$itemsCount=0;
+  										$Content = "";
+  										while($rsNextman){
+  										  $itemsCount= $itemsCount+1;
+  										  $Content .=sprintf($newTplItem,$rsNextman["title"],$rsNextman["description"],$rsNextman["picUrl"],$rsNextman["url"]);  										
+  											$rsNextman=mysql_fetch_assoc($result);  
+  										}
+ 										 
+							        $header = sprintf($newTplHeader,$itemsCount);
+							        $footer = sprintf($newTplFoot,$FuncFlag);
+							        echo $header . $Content . $footer; 													                     
+                     
+                   }
+		
+
+                	
+                }                  
+                 
+                 
+
+
     }
   }
 		
