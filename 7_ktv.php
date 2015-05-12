43612c52a4bc786bab7ff0ece273ebf45ede1f2d
wangbing <280877288@qq.com>
2015-05-12 18:34:29 +0800

    no message

---
+++ 7_ktv.php
@@ -0,0 +1,174 @@
+﻿<?php
+/**
+  * wechat php test
+  */
+
+//define your token
+define("TOKEN", "weixin888");
+$wechatObj = new wechatCallbackapiTest();
+//$wechatObj->valid();
+$wechatObj->responseMsg();
+
+class wechatCallbackapiTest
+{
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+
+        //valid signature , option
+        if($this->checkSignature()){
+        	echo $echoStr;
+        	exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+                
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $msgFromType= $postObj->MsgType;
+                 
+                if ($msgFromType=="event"){  
+                 	$contentStr = "欢迎来到微信MOA在线点歌系统：\r\n\r\n输入问号“?”或者“点歌”，可查看歌曲列表。\r\n\r\n输入歌曲的数字序号就可在线点歌"; 
+						        $msgType = "text";
+						        $time = time();
+				        		$textTpl = "<xml>
+														<ToUserName><![CDATA[%s]]></ToUserName>
+														<FromUserName><![CDATA[%s]]></FromUserName>
+														<CreateTime>%s</CreateTime>
+														<MsgType><![CDATA[%s]]></MsgType>
+														<Content><![CDATA[%s]]></Content>
+														<FuncFlag>0</FuncFlag>
+														</xml>"; 						        
+						        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+						        echo $resultStr;                   	
+
+                }
+                 
+                if ($msgFromType=="text"){ 
+                 	$keyword = trim($postObj->Content);
+                 	if ($keyword=="?"||$keyword=="？"||$keyword=="点歌"){ 
+                 		$contentStr = "歌曲列表：\r\n\r\n1、王菲-红豆\r\n2、王菲-棋子\r\n3、王菲-容易受伤的女人\r\n4、王菲-天空\r\n5、王菲-我愿意\r\n6、王菲-旋木\r\n7、王菲-执迷不悔\r\n\r\n输入以上歌曲的数字序号就可在线点歌"; 
+						        $msgType = "text";
+						        $time = time();
+				        		$textTpl = "<xml>
+														<ToUserName><![CDATA[%s]]></ToUserName>
+														<FromUserName><![CDATA[%s]]></FromUserName>
+														<CreateTime>%s</CreateTime>
+														<MsgType><![CDATA[%s]]></MsgType>
+														<Content><![CDATA[%s]]></Content>
+														<FuncFlag>0</FuncFlag>
+														</xml>"; 						        
+						        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+						        echo $resultStr;                 		
+
+                 	}else{
+                 	
+                 		if (is_numeric($keyword)){
+											switch ($keyword)
+											{
+											case "1":
+												$Description  = "1、红豆"; 
+												break;
+											case "2":
+											  $Description = "2、棋子";
+											  break;
+											case "3":
+											  $Description = "3、容易受伤的女人";
+											  break;
+											case "4":
+											  $Description = "4、天空";
+											  break;
+											case "5":
+											  $Description = "5、我愿意";
+											  break;
+											case "6":
+											  $Description = "6、旋木";
+											  break;	
+											case "7":
+											  $Description = "7、执迷不悔";
+											  break;												
+											default:
+												$Description  = "1、红豆";  
+												$keyword  = "1";
+											}
+											$MusicUrl = "http://www.hngsmlk.com.cn/MP3/".$keyword.".mp3";
+											
+											
+											
+			                $msgType="music";
+			                $textTpl = "<xml>
+											            <ToUserName><![CDATA[%s]]></ToUserName>
+																	<FromUserName><![CDATA[%s]]></FromUserName>
+																	<CreateTime>%s</CreateTime>
+																	<MsgType><![CDATA[%s]]></MsgType>
+																	<Music>
+																	<Title><![CDATA[%s]]></Title>
+																	<Description><![CDATA[王菲经典歌曲]]></Description>
+																	<MusicUrl><![CDATA[%s]]></MusicUrl>
+																	<HQMusicUrl><![CDATA[%s]]></HQMusicUrl>
+																	</Music>
+																	<FuncFlag>0</FuncFlag>
+																	</xml>";  
+											$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $Description, $MusicUrl, $MusicUrl);
+									    echo $resultStr;   																	
+                 		
+                 		}else{
+
+		                 		$contentStr = "歌曲列表：\r\n\r\n1、王菲-红豆\r\n2、王菲-棋子\r\n3、王菲-容易受伤的女人\r\n4、王菲-天空\r\n5、王菲-我愿意\r\n6、王菲-旋木\r\n7、王菲-执迷不悔\r\n\r\n输入以上歌曲的数字序号就可在线点歌"; 
+								        $msgType = "text";
+						        		$textTpl = "<xml>
+																<ToUserName><![CDATA[%s]]></ToUserName>
+																<FromUserName><![CDATA[%s]]></FromUserName>
+																<CreateTime>%s</CreateTime>
+																<MsgType><![CDATA[%s]]></MsgType>
+																<Content><![CDATA[%s]]></Content>
+																<FuncFlag>0</FuncFlag>
+																</xml>"; 						        
+								        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+								        echo $resultStr; 
+                 		
+                 		}
+               
+			 
+			 
+               		
+                 	}
+
+
+                	
+                }                 
+                 
+                 
+
+    }
+  }
+		
+	private function checkSignature()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
--- wx_sample.php
+++ wx_sample.php
@@ -4,7 +4,7 @@
   */
 
 //define your token
-define("TOKEN", "weixin888");
+define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
 //$wechatObj->valid();
 $wechatObj->responseMsg();
@@ -33,121 +33,33 @@ class wechatCallbackapiTest
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
                 $fromUsername = $postObj->FromUserName;
                 $toUsername = $postObj->ToUserName;
-                $msgFromType= $postObj->MsgType;
-                 
-                if ($msgFromType=="event"){  
-                 	$contentStr = "欢迎来到微信MOA在线点歌系统：\r\n\r\n输入问号“?”或者“点歌”，可查看歌曲列表。\r\n\r\n输入歌曲的数字序号就可在线点歌"; 
-						        $msgType = "text";
-						        $time = time();
-				        		$textTpl = "<xml>
-														<ToUserName><![CDATA[%s]]></ToUserName>
-														<FromUserName><![CDATA[%s]]></FromUserName>
-														<CreateTime>%s</CreateTime>
-														<MsgType><![CDATA[%s]]></MsgType>
-														<Content><![CDATA[%s]]></Content>
-														<FuncFlag>0</FuncFlag>
-														</xml>"; 						        
-						        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-						        echo $resultStr;                   	
-
-                }
-                 
-                if ($msgFromType=="text"){ 
-                 	$keyword = trim($postObj->Content);
-                 	if ($keyword=="?"||$keyword=="？"||$keyword=="点歌"){ 
-                 		$contentStr = "歌曲列表：\r\n\r\n1、王菲-红豆\r\n2、王菲-棋子\r\n3、王菲-容易受伤的女人\r\n4、王菲-天空\r\n5、王菲-我愿意\r\n6、王菲-旋木\r\n7、王菲-执迷不悔\r\n\r\n输入以上歌曲的数字序号就可在线点歌"; 
-						        $msgType = "text";
-						        $time = time();
-				        		$textTpl = "<xml>
-														<ToUserName><![CDATA[%s]]></ToUserName>
-														<FromUserName><![CDATA[%s]]></FromUserName>
-														<CreateTime>%s</CreateTime>
-														<MsgType><![CDATA[%s]]></MsgType>
-														<Content><![CDATA[%s]]></Content>
-														<FuncFlag>0</FuncFlag>
-														</xml>"; 						        
-						        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-						        echo $resultStr;                 		
-
-                 	}else{
-                 	
-                 		if (is_numeric($keyword)){
-											switch ($keyword)
-											{
-											case "1":
-												$Description  = "1、红豆"; 
-												break;
-											case "2":
-											  $Description = "2、棋子";
-											  break;
-											case "3":
-											  $Description = "3、容易受伤的女人";
-											  break;
-											case "4":
-											  $Description = "4、天空";
-											  break;
-											case "5":
-											  $Description = "5、我愿意";
-											  break;
-											case "6":
-											  $Description = "6、旋木";
-											  break;	
-											case "7":
-											  $Description = "7、执迷不悔";
-											  break;												
-											default:
-												$Description  = "1、红豆";  
-												$keyword  = "1";
-											}
-											$MusicUrl = "http://www.hngsmlk.com.cn/MP3/".$keyword.".mp3";
-											
-											
-											
-			                $msgType="music";
-			                $textTpl = "<xml>
-											            <ToUserName><![CDATA[%s]]></ToUserName>
-																	<FromUserName><![CDATA[%s]]></FromUserName>
-																	<CreateTime>%s</CreateTime>
-																	<MsgType><![CDATA[%s]]></MsgType>
-																	<Music>
-																	<Title><![CDATA[%s]]></Title>
-																	<Description><![CDATA[王菲经典歌曲]]></Description>
-																	<MusicUrl><![CDATA[%s]]></MusicUrl>
-																	<HQMusicUrl><![CDATA[%s]]></HQMusicUrl>
-																	</Music>
-																	<FuncFlag>0</FuncFlag>
-																	</xml>";  
-											$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $Description, $MusicUrl, $MusicUrl);
-									    echo $resultStr;   																	
-                 		
-                 		}else{
-
-		                 		$contentStr = "歌曲列表：\r\n\r\n1、王菲-红豆\r\n2、王菲-棋子\r\n3、王菲-容易受伤的女人\r\n4、王菲-天空\r\n5、王菲-我愿意\r\n6、王菲-旋木\r\n7、王菲-执迷不悔\r\n\r\n输入以上歌曲的数字序号就可在线点歌"; 
-								        $msgType = "text";
-						        		$textTpl = "<xml>
-																<ToUserName><![CDATA[%s]]></ToUserName>
-																<FromUserName><![CDATA[%s]]></FromUserName>
-																<CreateTime>%s</CreateTime>
-																<MsgType><![CDATA[%s]]></MsgType>
-																<Content><![CDATA[%s]]></Content>
-																<FuncFlag>0</FuncFlag>
-																</xml>"; 						        
-								        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-								        echo $resultStr; 
-                 		
-                 		}
-               
-			 
-			 
-               		
-                 	}
+                $msgType = $postObj->MsgType;
 
 
-                	
-                }                 
-                 
-                 
-
+                $textTpl = " <xml>
+ 															<ToUserName><![CDATA[%s]]></ToUserName>
+  														<FromUserName><![CDATA[%s]]></FromUserName>
+  														<CreateTime>%s</CreateTime>
+  														<MsgType><![CDATA[news]]></MsgType>
+  														<ArticleCount>2</ArticleCount>
+  															<Articles>
+  															  <item>
+  															    <Title><![CDATA[图片一]]></Title> 
+  															    <Description><![CDATA[图片一描述]]></Description>
+  															    <PicUrl><![CDATA[http://www.image1.cn/upload/docnews/12/20130527/12_1369639156.jpg]]></PicUrl>
+  															    <Url><![CDATA[http://www.image1.cn/h/12/20130521/14685_5.html]]></Url>
+  															  </item>
+  															  <item>
+  															    <Title><![CDATA[图片二]]></Title> 
+  															    <Description><![CDATA[图片二描述]]></Description>
+  															    <PicUrl><![CDATA[http://www.image1.cn/upload/docnews/12/20130521/p17r36qv371jpt1j9u17kp49f15ub6.jpg]]></PicUrl>
+  															    <Url><![CDATA[http://www.image1.cn/h/12/20130521/14685_3.html]]></Url>
+  															  </item>
+  														  </Articles>
+   														<FuncFlag>1</FuncFlag>
+  													 </xml> "; 
+								$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time);
+						    echo $resultStr; 
     }
   }
 		
