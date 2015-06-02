d880f873a7b2223c0053a9901eb13e8ee3bbd544
wangbing <280877288@qq.com>
2015-06-02 18:28:31 +0800

    no message

--- sample.php
+++ sample.php
@@ -2,7 +2,7 @@
 
 $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-//file_put_contents('demo.txt', $postStr);
+file_put_contents('/var/www/html/demo.txt', $postStr);
 
-var_dump($postStr);
+//var_dump($postStr);
 
