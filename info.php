a8594c92d4a8f52d78c8433eff54fab7866b96dd
wangbing <280877288@qq.com>
2015-07-02 18:03:31 +0800

    no message

--- info.php
+++ info.php
@@ -1,6 +1,5 @@
 <?php
 phpinfo();
-var_dump($expression);
 ?>
 
 
---
+++ wx_sample.php
@@ -0,0 +1,12 @@
+<?php
+
+//$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+//file_put_contents('/tmp/demo.txt', $postStr);
+
+//var_dump($postStr);
+
+
+include '/var/www/html/mem.php';
+
+var_dump(mem_token());
